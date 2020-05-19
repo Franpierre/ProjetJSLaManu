700381e5b5afc329ca654fbdf36ef12dad7882c9
FabienMarti <martiretti.fabien@gmail.com>
2020-05-19 11:21:53 +0200

    merge et finitions

--- assets/js/script.js
+++ assets/js/script.js
@@ -88,8 +88,117 @@ let canard6 = new Canard(
     'assets/img/classic/classicBlue.jpg',
     1,
 )
+let canard7 = new Canard(
+    'Canard custom baby',
+    20,
+    'Petit bébé canard 12cm x 8cm',
+    '006',
+    'Special',
+    'assets/img/special/babyCustom.jpg',
+    1,
+)
+let canard8 = new Canard(
+    'Canard custom doc ',
+    20,
+    'Petit docteur canard 12cm x 8cm',
+    '007',
+    'Special',
+    'assets/img/special/docCustom.jpg',
+    1,
+)
+let canard9 = new Canard(
+    'Canard custom french',
+    20,
+    'Petit canard français 12cm x 8cm',
+    '008',
+    'Special',
+    'assets/img/special/frenchCustom.jpg',
+    1,
+)
+let canard10 = new Canard(
+    'Canard custom king',
+    20,
+    'Petit Roi canard 12cm x 8cm',
+    '009',
+    'Special',
+    'assets/img/special/kingCustom.jpg',
+    1,
+)
+let canard11 = new Canard(
+    'Canard custom pirate',
+    20,
+    'Petit canard pirate 12cm x 8cm',
+    '010',
+    'Special',
+    'assets/img/special/pirateCustom.jpg',
+    1,
+)
+let canard12 = new Canard(
+    'Canard custom punk',
+    20,
+    'Petit canard Punk 12cm x 8cm',
+    '011',
+    'Special',
+    'assets/img/special/punkCustom.jpg',
+    1,
+)
+let canard13 = new Canard(
+    'Canard led bleu',
+    50,
+    'Petit Canard led bleu 12cm x 8cm',
+    '012',
+    'Special',
+    'assets/img/led/ledBlue.jpg',
+    1,
+)
+let canard14 = new Canard(
+    'Canard led jaune',
+    50,
+    'Canard led jaune 12cm x 8cm',
+    '013',
+    'Special',
+    'assets/img/led/ledYellow.jpg',
+    1,
+)
+let canard15 = new Canard(
+    'Canard led vert',
+    50,
+    'Canard led vert 12cm x 8cm',
+    '014',
+    'Special',
+    'assets/img/led/ledGreen.jpg',
+    1,
+)
+let canard16 = new Canard(
+    'Canard led rose',
+    50,
+    'Canard led rose 12cm x 8cm',
+    '015',
+    'Special',
+    'assets/img/led/ledPink.jpg',
+    1,
+)
+let canard17 = new Canard(
+    'Canard led rouge',
+    50,
+    'Canard led rouge 12cm x 8cm',
+    '016',
+    'Special',
+    'assets/img/led/ledRed.jpg',
+    1,
+)
+let canard18 = new Canard(
+    'Canard led blanc',
+    50,
+    'Canard led blanc 12cm x 8cm',
+    '017',
+    'Special',
+    'assets/img/led/ledWhite.jpg',
+    1,
+)
+
 //Fonction Set
-var allDucks = [canard1, canard2, canard3, canard4, canard5, canard6];
+var allDucks = [canard1, canard2, canard3, canard4, canard5, canard6, canard7, canard8, canard9, canard10, canard11, canard12, canard13, canard14, canard15, canard16, canard17, canard18];
 
 var selectedDuck;
 function setDuck(activeDuck) {
